69ea4e6b2f2777edcaf46b8dc30b40a24d29e515
iliasMahraoui <2143218@talnet.nl>
2022-02-06 16:21:06 +0100

    1

---
+++ 2-Loops/Opdracht-2.3/loops-foreach-deel1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+echo "<ul>";
+foreach( $dagen as $days ){
+ echo "<li>". $days . "</li>";   
+} echo "</ul>";
+
+?> 
+
+</body>
+</html>
