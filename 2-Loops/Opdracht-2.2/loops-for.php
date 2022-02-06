9df40374e48f69803f7ad295662aef57cbaf2e2c
iliasMahraoui <2143218@talnet.nl>
2022-02-06 16:07:16 +0100

    1

---
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<ul>";
+for($b = 0; $b <= 18; $b++ ){
+    
+    echo  "<li>ik ben". " " . $b. "jaar, dus ik mag nog niet stemmen</li>";
+   
+}
+echo  "<li>ik ben 18 jaar oud dus ik heb stemrecht! </li>";
+echo "</ul>";
+?>
+
+</body>
+</html>
