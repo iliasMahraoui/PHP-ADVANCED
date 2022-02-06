4d3b506927af4251390c64f250617153edb3cb62
iliasMahraoui <2143218@talnet.nl>
2022-02-06 15:10:08 +0100

    1

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+
+$persoon["voornaam"] = "Ilias";
+$persoon["achternaam"] = "Mahraoui";
+$persoon["leeftijd"] = 17;
+$persoon["opleiding"] = "Software Developer";
+
+echo $persoon['voornaam']. " ". $persoon['achternaam'] . " is " . $persoon['leeftijd']. " jaar oud en is student aan het ROC van Amsterdam en ik ben een " . $persoon['opleiding'] ; 
+
+?>
+
+
+</body>
+</html>
