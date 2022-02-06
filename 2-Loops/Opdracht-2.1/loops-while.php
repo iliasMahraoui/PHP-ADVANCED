6488c03850886413bd8b30f4898e340590c20371
iliasMahraoui <2143218@talnet.nl>
2022-02-06 16:00:00 +0100

    1

---
+++ 2-Loops/Opdracht-2.1/loops-while.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+$leeftijd = 0;
+
+while( $leeftijd < 18){
+echo "ik ben". " " .$leeftijd. " jaar, dus ik mag nog niet stemmen,<br>";
+$leeftijd++;
+
+}
+echo "ik ben 18 jaar oud, dus ik mag nu stemmen <br>";
+
+?>
+</body>
+</html>
