3619502c579c30e3d54ef2e6ef38cc061c1a4344
iliasMahraoui <2143218@talnet.nl>
2022-02-06 16:26:53 +0100

    1

---
+++ 2-Loops/Opdracht-2.4/loops-foreach-deel2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<form action="verzondeinfo.php">
+    <label for="klas">ik zit in klas:</label>
+<select>
+    <?php 
+    $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    foreach ($klassen as $klas) {
+     ?>
+    <option><?php echo $klas ?> </option>
+    <?php } ?>
+ </select>
+  <input type="submit" name="submit" value="verzenden">  
+</form>
+</body>
+</html>
