7c6885cc2654eafff6f5a14d17c9f656db4a77d4
iliasMahraoui <2143218@talnet.nl>
2022-02-06 15:08:17 +0100

    1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+$gegevens = array ("voornaam" => "Ilias", "Achternaam" => "Mahraoui", "Leeftijd" => "17", "klas" => "1C");
+echo "Voornaam : ". $gegevens["voornaam"];
+echo "<br>";
+echo "Achternaam : ". $gegevens["Achternaam"];
+echo "<br>";
+echo "Leeftijd : " . $gegevens["Leeftijd"]. " jaar " ;
+echo "<br>";
+echo "Klas : ", $gegevens["klas"];
+?>
+
+</body>
+</html>
