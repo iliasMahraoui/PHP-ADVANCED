ecabfad5526de379b2855e3ff3beadf36676fc37
iliasMahraoui <2143218@talnet.nl>
2022-02-17 14:15:47 +0100

    1

---
+++ 3-Forms/Opdracht-3.3/mijngegevens.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<ol>
+  <li>Artiest: <?php echo $_GET['artiest'] ?></li>
+  <li>Titel: <?php echo $_GET['titel']?></li>
+  <li>Jaar van uitkomst: <?php echo $_GET['JaarVanUitkomst']?></li>
+  <li>Muzieksoort: <?php echo $_GET['muzieksoort']?></li>
+</ol>
+
+
+<!-- mijngegevens.php?
+artiest=Imagine Dragons&
+titel=Enemy&
+JaarVanUitkomst=2019&
+muzieksoort=Pop Rock -->
+
+
+</body>
+</html>
---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>Mijn favoriete nummer:</h3>
+<form name id="muziek" action = "mijngegevens.php" method="get">
+<button>
+<a href="mijngegevens.php?
+artiest=Imagine Dragons&
+titel=Enemy&
+JaarVanUitkomst=2019&
+muzieksoort=Pop Rock">Meer informatie</a>
+</button>
+</form>
+</body>
+</html>
